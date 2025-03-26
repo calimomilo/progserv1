7d7a318ad6fc81cb43749ed49b2a97fb9687f2a9
Camilo <paula@pena-parra.ch>
2025-03-26 09:38:06 +0100

    cours 2 (mini-projet + serie)

--- mini-projet/index.php
+++ mini-projet/index.php
@@ -1,2 +1,27 @@
 <?php
-echo "Bienvenue dans le mini-projet de l'unité d'enseignement Programmation serveur 1 !";
+// echo "Bienvenue dans le mini-projet de l'unité d'enseignement Programmation serveur 1 !";
+require "functions.php";
+addPet("Caramel", 3);
+addPet("Rex", 8);
+addPet("Tweety", 1);
+addPet("Godzilla", 4);
+getPets();
+getPet("Rex");
+updatePet("Rex", 9);
+removePet("Tweety");
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Page d'accueil | Gestionnaire d'animaux de compagnie</title>
+</head>
+
+<body>
+<h1>Page d'accueil du gestionnaire d'animaux de compagnie</h1>
+<p>Bienvenue sur la page d'accueil du gestionnaire d'animaux de compagnie !</p>
+<p>Utilise cette page pour visualiser et gérer tous les animaux de compagnie.</p>
+</body>
+
+</html>
