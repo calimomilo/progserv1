7e30c4637ee6713e69005b16fe8b382e0515e483
Camilo <paula@pena-parra.ch>
2025-06-23 13:35:14 +0200

    cours 5 exercices

--- exercices/serie5/index.php
+++ exercices/serie5/index.php
@@ -1,5 +1,7 @@
 <?php
 
+//EXERCICE 1A
+
 const DATABASE_FILE = './grades.db';
 $pdo = new PDO("sqlite:" . DATABASE_FILE);
 
@@ -11,6 +13,8 @@ $sql = "CREATE TABLE IF NOT EXISTS courses (
 );";
 
 $pdo->exec($sql);
+
+//EXERCICE 1B
 
 function addGrade($name, $grade, $acronym = null){
     global $pdo;
@@ -30,3 +34,107 @@ $droit1Id = addGrade('Droit des médias 1', 4.1, 'Droit1');
 $introDuraId = addGrade('Introduction à la durabilité', 4.4, 'IntroDura');
 $progServ1Id = addGrade('Programmation serveur 1', 5.3, 'ProgServ1');
 
+//EXERCICE 1C -> YA RIEN QUI S'AFFICHE
+
+function getGrade($id){
+    global $pdo;
+
+    $sql = "SELECT * FROM courses WHERE id = $id";
+
+    $grade = $pdo->query($sql);
+
+    $grade = $grade->fetch();
+
+    return $grade;
+}
+
+$progServ1 = getGrade($progServ1Id);
+
+if ($progServ1) {
+    // On affiche le cours récupéré
+    echo "<h1>Informations sur le cours</h1>";
+    echo "<p><strong>Identifiant</strong> : " . $progServ1['id'] . "</p>";
+    echo "<p><strong>Nom</strong> : " . $progServ1['name'] . "</p>";
+    echo "<p><strong>Acronyme</strong> : " . $progServ1['acronym'] . "</p>";
+    echo "<p><strong>Note</strong> : " . $progServ1['grade'] . "</p>";
+} else {
+    echo "<p>Aucun cours trouvé avec cet identifiant.</p>";
+}
+
+$notFound = getGrade(9999);
+
+if ($notFound) {
+    // On affiche le cours récupéré
+    echo "<h1>Informations sur le cours</h1>";
+    echo "<p><strong>Identifiant</strong> : " . $notFound['id'] . "</p>";
+    echo "<p><strong>Nom</strong> : " . $notFound['name'] . "</p>";
+    echo "<p><strong>Acronyme</strong> : " . $notFound['acronym'] . "</p>";
+    echo "<p><strong>Note</strong> : " . $notFound['grade'] . "</p>";
+} else {
+    echo "<p>Aucun cours trouvé avec cet identifiant.</p>";
+}
+
+//EXERCICE 1D
+
+function getGrades(){
+    global $pdo;
+
+    $sql = "SELECT * FROM courses";
+
+    $grades = $pdo->query($sql);
+
+    $grades = $grades->fetchAll();
+
+    return $grades;
+}
+
+$grades = getGrades();
+
+?>
+<h1>Liste des cours</h1>
+<table>
+    <tr>
+        <th>Identifiant</th>
+        <th>Nom</th>
+        <th>Acronyme</th>
+        <th>Note</th>
+    </tr>
+
+    <?php foreach ($grades as $grade): ?>
+        <tr>
+            <td><?php echo $grade['id']; ?></td>
+            <td><?php echo $grade['name']; ?></td>
+            <td><?php echo $grade['acronym']; ?></td>
+            <td><?php echo $grade['grade']; ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
+
+<?php
+
+// EXERCICE 1E
+
+function removeGrade($id){
+    global $pdo;
+
+    $sql = "DELETE FROM courses WHERE id = $id";
+
+    return $pdo->exec($sql);
+}
+
+$numberOfAffectedRows = removeGrade($baseProg2Id);
+
+if ($numberOfAffectedRows == 1) {
+    echo "<p>Le cours avec l'identifiant $baseProg2Id a été supprimé avec succès.</p>";
+} else {
+    echo "<p>Erreur lors de la suppression du cours avec l'identifiant $baseProg2Id.</p>";
+}
+
+// On essaie de supprimer un cours avec un identifiant qui n'existe pas
+$numberOfAffectedRows = removeGrade(9999);
+
+if ($numberOfAffectedRows == 1) {
+    echo "<p>Le cours avec l'identifiant 9999 a été supprimé avec succès.</p>";
+} else {
+    echo "<p>Aucun cours trouvé avec cet identifiant (9999).</p>";
+}
